7ae97bf2996c4f044dfd91355c02a406af34c256
Amritanshu <amritanshuj.ic.16@nsit.net.in>
2020-09-01 04:06:44 +0530

    Setting up Multer and Initializing Routes

--- config/db.js
+++ config/db.js
@@ -1,17 +1,16 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
-
-function connectDB(){
-    //Database connection
-    mongoose.connect(process.env.MONGO_CONNECTION_URL, {useNewUrlParser: true, useCreateIndex:true, useUnifiedTopology: 
-    true, useFindAndModiify: true});
+function connectDB() {
+    // Database connection 🥳
+    mongoose.connect(process.env.MONGO_CONNECTION_URL, { useNewUrlParser: true, useCreateIndex:true, useUnifiedTopology: true, useFindAndModify : true });
     const connection = mongoose.connection;
-
-    connection.once('open', ()=>{
-        console.log('Database connected!');
+    connection.once('open', () => {
+        console.log('Database connected');
     }).catch(err => {
         console.log('Connection failed');
-    })
+    });
 }
+
+// mIAY0a6u1ByJsWWZ
 
 module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 const PORT = process.env.PORT || 3000;
 
-const connectDB = require('./config/db.js');
+const connectDB = require('./config/db');
 connectDB();  
+
+//Template Engine
+app.set('views', path.join(__dirname, '/views'));
+app.set('view engine', 'ejs');
+
+//Routes
+app.use('/api/files', require('./routes/files'))
+app.use('/files', require('./routes/show'));
+
 
 app.listen(PORT, function(err){
     if(err){
