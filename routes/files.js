4cff354d711c2cd7b8f62a91a88deb5181e96b45
Amritanshu <amritanshuj.ic.16@nsit.net.in>
2020-09-04 20:34:26 +0530

    Setting up SMTP Service

--- routes/files.js
+++ routes/files.js
@@ -49,4 +49,41 @@ router.post('/', (req, res) => {
     //Response -> Link
 });
 
+router.post('/send', async (req, res)=>{
+    const {uuid, emailTo, emailFrom} = req.body;
+    //Validate request
+    if(!uuid || !emailTo || !emailFrom){
+        return res.status(422).send({error: 'All fields are required'});
+    }
+
+    //Get data from database
+    const file = await File.findOne({uuid: uuid});
+    if(file.sender){
+        return res.status(422).send({error: 'Email already sent'});
+    }
+
+    file.sender = emailFrom;
+    file.receiver = emailTo;
+    const response = await file.save();
+
+    //Send email
+    const sendMail = require('../services/emailService');
+    sendMail({
+        from: emailFrom,
+        to: emailTo,
+        subject: 'QuickShare File Sharing',
+        text: `${emailFrom} shared a file with you!`,
+        html: require('../services/emailTemplate')({
+            emailFrom: emailFrom,
+            downloadLink: `${process.env.APP_BASE_URL}/files/${file.uuid}`,
+            size: parseInt(file.size/1000) + 'KB',
+            expires: '24 hours'
+        })
+
+    });
+
+    return res.send({success: true});
+
+})
+
 module.exports = router;
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const path = require('path');
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static('public'));
+app.use(express.json());
 
 const connectDB = require('./config/db');
 connectDB();  
