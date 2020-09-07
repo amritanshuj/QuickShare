1706b00322280103d0f35ac20961f06569f9912f
Amritanshu <amritanshuj.ic.16@nsit.net.in>
2020-09-07 21:16:00 +0530

    Fixing bugs

--- public/js/index.js
+++ public/js/index.js
@@ -124,7 +124,7 @@ const onFileUploadSuccess = (res) => {
   emailForm[2].innerText = "Send";
   progressContainer.style.display = "none"; // hide the box
 
-  const { files: url } = JSON.parse(res);
+  const { files[0]: url } = JSON.parse(res);
   console.log(url);
   sharingContainer.style.display = "block";
   fileURL.value = url;
