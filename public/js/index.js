5fedd54e63edc9eda2650138ba68d9fa12d037c0
Amritanshu <amritanshuj.ic.16@nsit.net.in>
2020-09-08 01:49:55 +0530

    Fixing File Upload

--- public/js/index.js
+++ public/js/index.js
@@ -26,57 +26,6 @@ browseBtn.addEventListener("click", () => {
   fileInput.click();
 });
 
-dropZone.addEventListener("drop", (e) => {
-  e.preventDefault();
-  //   console.log("dropped", e.dataTransfer.files[0].name);
-  const files = e.dataTransfer.files;
-  console.log('***:', files);
-  if (files.length === 1) {
-    if (files[0].size < maxAllowedSize) {
-      fileInput.files = files;
-      uploadFile();
-    } else {
-      showToast("Max file size is 100MB");
-    }
-  } else if (files.length > 1) {
-    showToast("You can't upload multiple files");
-  }
-  dropZone.classList.remove("dragged");
-});
-
-dropZone.addEventListener("dragover", (e) => {
-  e.preventDefault();
-  dropZone.classList.add("dragged");
-
-  // console.log("dropping file");
-});
-
-dropZone.addEventListener("dragleave", (e) => {
-  dropZone.classList.remove("dragged");
-
-  console.log("drag ended");
-});
-
-// file input change and uploader
-fileInput.addEventListener("change", () => {
-  if (fileInput.files[0].size > maxAllowedSize) {
-    showToast("Max file size is 100MB");
-    fileInput.value = ""; // reset the input
-    return;
-  }
-  uploadFile();
-});
-
-// sharing container listenrs
-copyURLBtn.addEventListener("click", () => {
-  fileURL.select();
-  document.execCommand("copy");
-  showToast("Copied to clipboard");
-});
-
-fileURL.addEventListener("click", () => {
-  fileURL.select();
-});
 
 const uploadFile = () => {
   console.log("file added uploading");
@@ -132,6 +81,60 @@ const onFileUploadSuccess = (res) => {
   sharingContainer.style.display = "block";
   fileURL.value = url;
 };
+
+
+dropZone.addEventListener("drop", (e) => {
+  e.preventDefault();
+  //   console.log("dropped", e.dataTransfer.files[0].name);
+  const files = e.dataTransfer.files;
+  console.log('***:', files);
+  if (files.length === 1) {
+    if (files[0].size < maxAllowedSize) {
+      fileInput.files = files;
+      uploadFile();
+    } else {
+      showToast("Max file size is 100MB");
+    }
+  } else if (files.length > 1) {
+    showToast("You can't upload multiple files");
+  }
+  dropZone.classList.remove("dragged");
+});
+
+dropZone.addEventListener("dragover", (e) => {
+  e.preventDefault();
+  dropZone.classList.add("dragged");
+
+  // console.log("dropping file");
+});
+
+dropZone.addEventListener("dragleave", (e) => {
+  dropZone.classList.remove("dragged");
+
+  console.log("drag ended");
+});
+
+// file input change and uploader
+fileInput.addEventListener("change", () => {
+  if (fileInput.files[0].size > maxAllowedSize) {
+    showToast("Max file size is 100MB");
+    fileInput.value = ""; // reset the input
+    return;
+  }
+  uploadFile();
+});
+
+// sharing container listenrs
+copyBtn.addEventListener("click", () => {
+  fileURL.select();
+  document.execCommand("copy");
+  showToast("Copied to clipboard");
+});
+
+fileURL.addEventListener("click", () => {
+  fileURL.select();
+});
+
 
 emailForm.addEventListener("submit", (e) => {
   e.preventDefault(); // stop submission
