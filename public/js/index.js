8b2734c0e1a8de6dd61bdd8bfcc0c21da949bef1
Amritanshu <amritanshuj.ic.16@nsit.net.in>
2020-09-10 23:02:37 +0530

    Debugging File Upload

--- public/js/index.js
+++ public/js/index.js
@@ -15,7 +15,7 @@ const emailForm = document.querySelector("#emailForm");
 
 const toast = document.querySelector(".toast");
 
-const baseURL = process.env.APP_BASE_URL;
+const baseURL = "https://sharequick.herokuapp.com";
 const uploadURL = `${baseURL}/api/files`;
 const emailURL = `${baseURL}/api/files/send`;
 
