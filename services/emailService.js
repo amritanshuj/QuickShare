f3fd6231c0af394f6db5ee785ed97addd99cede8
Amritanshu <amritanshuj.ic.16@nsit.net.in>
2020-09-15 02:34:40 +0530

    Fixing Mail Service

--- services/emailService.js
+++ services/emailService.js
@@ -1,10 +1,9 @@
 const nodemailer = require('nodemailer');
 
 async function sendMail({from, to, subject, text, html}){
-    let transporter = nodemailer.createTransport({  
-        service:'gmail', 
-        host:'smtp.gmail.com', 
-        port: 587,
+    let transporter = nodemailer.createTransport({
+        host: process.env.SMTP_HOST,
+        port: process.env.SMTP_PORT,
         secure: false,
         auth:{
             user: process.env.MAIL_USER,
